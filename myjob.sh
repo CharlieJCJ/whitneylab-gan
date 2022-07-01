651f1117098da05c2fda46ec70ccbd476e8ef565
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-06-30 19:26:22 -0700

    add

---
+++ myjob.sh
@@ -0,0 +1,15 @@
+    #!/bin/bash
+    # Job name:
+    #SBATCH --job-name=test
+    #
+    # Account:
+    #SBATCH --account=charliechengjieji
+    #
+    # Partition:
+    #SBATCH --partition=savio2
+    #
+    # Wall clock limit:
+    #SBATCH --time=00:00:30
+    #
+    ## Command(s) to run:
+    echo "hello world"
