8742864d8870c7b47e45807de429ee2940022b28
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-07-19 23:29:03 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # whitneylab-gan
+
+- storing miscellaneous files for the whitneylab-gan project
+- for example, the example job scripts, etc.
+- `myjob.sh`: small tasks
+- `gpujob.sh`: large tasks (requiring a GPUs)
--- myjob.sh
+++ myjob.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=test
+#SBATCH --job-name=styleGAN2ada (single class training)
 #
 # Account:
 #SBATCH --account=fc_psychvis
 #
+# QoS: must be savio_long for jobs &gt; 3 days
+#SBATCH --qos=savio_long
+#
 # Partition:
 #SBATCH --partition=savio2_htc
 #
-# Wall clock limit:
-#SBATCH --time=01:00:00
+# Number of tasks needed for use case (example):
+#SBATCH --ntasks=2
 #
-## Command(s) to run:
-echo "hello world"
+# Processors per task:
+#SBATCH --cpus-per-task=1
+#
+# Wall clock limit (7 days in this case):
+#SBATCH --time=7-00:00:00
+#
+## Command(s) to run (example):
+python
